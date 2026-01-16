b7b59140110431407252ba5409d82b4af9950da5
LORD | CRAZY_JOKER67 <seleznevandrey186@outlook.com>
2026-01-16 16:20:53 +0700

    Fix line 812

--- envsetup.sh
+++ envsetup.sh
@@ -1,3 +1,4 @@
+
 # Copyright (C) 2022 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -809,6 +810,15 @@ function croot()
 }
 
 function _croot()
+{
+    local T=$(gettop)
+    if [ "$T" ]; then
+        local cur="${COMP_WORDS[COMP_CWORD]}"
+        k=0
+        for c in $(compgen -d ${T}/${cur}); do
+            COMPREPLY[k++]=${c#${T}/}/
+        done
+    fi
 }
 
 function cproj()
